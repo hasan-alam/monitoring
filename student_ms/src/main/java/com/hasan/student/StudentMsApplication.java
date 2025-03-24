144701b91a24a3ccc68c25d99f5762ba9510d1c9
hasan-alam <hasan.bin.alam.tanim@gmail.com>
2025-03-24 23:37:24 +0600

    management endpoints are configured

--- student_ms/src/main/java/com/hasan/student/StudentMsApplication.java
+++ student_ms/src/main/java/com/hasan/student/StudentMsApplication.java
@@ -3,10 +3,7 @@ package com.hasan.student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableFeignClients
