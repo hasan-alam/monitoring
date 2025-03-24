dbe9c8579718945ca6555c8700835f6a22a5962e
hasan-alam <hasan.bin.alam.tanim@fmail.com>
2025-03-24 15:43:54 +0600

    Service discovery and registry done

--- course_ms/src/main/java/com/hasan/course/CourseMsApplication.java
+++ course_ms/src/main/java/com/hasan/course/CourseMsApplication.java
@@ -2,8 +2,10 @@ package com.hasan.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//@EnableDiscoveryClient
 public class CourseMsApplication {
 
 	public static void main(String[] args) {
--- student_ms/src/main/java/com/hasan/student/StudentMsApplication.java
+++ student_ms/src/main/java/com/hasan/student/StudentMsApplication.java
@@ -3,6 +3,7 @@ package com.hasan.student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 @EnableFeignClients
 @EnableAutoConfiguration
+//@EnableDiscoveryClient
 public class StudentMsApplication {
 
 	public static void main(String[] args) {
