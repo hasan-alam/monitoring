72f52cc52b624f5a6d1b55679ae9a5e239edf17a
hasan-alam <hasan.bin.alam.tanim@fmail.com>
2025-03-25 23:34:11 +0600

    Persistent volumes are used and eureka server is added in docker compose

--- student_ms/src/main/java/com/hasan/student/StudentMsApplication.java
+++ student_ms/src/main/java/com/hasan/student/StudentMsApplication.java
@@ -3,12 +3,13 @@ package com.hasan.student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableAutoConfiguration
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class StudentMsApplication {
 
 	public static void main(String[] args) {
